3a4e321019a67aa610bf44edeb99d972d27b9c1b
Avyukt Ballari <ballariavitiktok@gmail.com>
2024-01-06 13:13:19 -0500

    removed unwanted routes

--- app.py
+++ app.py
@@ -29,36 +29,6 @@ def register():
 def started():
     return render_template('started.html', started=True)
 
-@app.route('/upload_pdf', methods=['POST'])
-def upload_pdf():
-    try:
-        data = request.get_json()
-        pdf_data = data.get('pdfData')
-
-        # Decode base64 and create a PDF document
-        pdf_bytes = pdf_data.split(',')[1].encode('utf-8')
-        pdf_document = fitz.open(stream=pdf_bytes, filetype="pdf")
-
-        # Extract text from the PDF
-        text = ""
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document[page_num]
-            text += page.get_text()
-
-        pdf_document.close()
-
-        # Redirect to problems.html with the extracted text
-        return redirect(url_for('problems', text=text))
-
-    except Exception as pdf_error:
-        print(f'Error processing PDF: {str(pdf_error)}')
-    return jsonify({'success': False, 'message': f'Error processing PDF: {str(pdf_error)}'})
-
-
-@app.route('/problems')
-def problems():
-    text = request.args.get('text', '')  # Get the extracted text from the URL parameter
-    return render_template('problems.html', text=text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
